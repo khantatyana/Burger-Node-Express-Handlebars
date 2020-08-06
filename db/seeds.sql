8cee1ffdf093a7cade63b11f507d1b2f369d72b0
Tatyana <hanyatan@gmail.com>
2020-08-05 21:31:57 -0400

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesburger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Macburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kingburger', false);
+
